5a92496c2dd2dca386349d3bf1fecfb0548179b0
Jon Davis <jon@snowulf.com>
2015-09-30 15:53:55 -0700

    UI Works - Crazy, but it works.
    Yes. It’s absolutely insane in my book to define form specific UI in
    the DB schema. But it works… and at this point in time that’s good
    enough.

--- client/templates/dynamic/recipSubmit.js
+++ client/templates/dynamic/recipSubmit.js
@@ -2,29 +2,9 @@
 // https://github.com/aldeed/meteor-autoform/blob/devel/templates/bootstrap3/components/afArrayField/afArrayField.html
 //
 
-Template.afArrayField_custom_ingredients.helpers({
-    unitOptions: function() {
-      var units = UnitsList.find({}).fetch();
-      var theList = [];
-      for(var u of units){
-        theList.push({label:u.name,value:u._id});
-      }
-      return theList;
-    }
-});
-
-
 Template.afArrayField_custom_ingredients.events({
   "autocompleteselect input": function(event, template, doc) {
     console.log("selected ", doc._id);
     $(this).siblings("input[name='id']").val(doc._id);
-  }
-});
-
-AutoForm.hooks({
-  newDrinkForm: {
-    onSubmit: function (insertDoc, updateDoc, currentDoc) {
-      console.log(JSON.stringify(insertDoc));
-    }
   }
 });
